e151e8f4b5143abb2a4260b3b82d57465cc97ea2
Jimmy Tang <2301714T@student.gla.ac.uk>
2017-02-02 14:53:01 +0000

    up to the html part of chapter 7 forms

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 """	#############################################
 def detail(request, question_id):
@@ -21,7 +22,30 @@ def about(request):
 	# Constructs an about page.
 	context_dict = {'boldmessage': "This is the __about__ page!! Yayyy"}
 	return render(request, 'rango/about.html', context=context_dict)
-	return HttpResponse("Rango says hey there partner!")
+	
+def add_category(request):
+	form = CategoryForm()
+	
+	# A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		#Have we been provided with a valid form?
+		if form.is_valid():
+			# Save the new category to the database.
+			form.save(commit=True)
+			# Now that the category is aved
+			# We could give a confirmation message
+			# but since the most recent category added is on the index page
+			# Then we can direct the user back to the index page.
+			return index(request)
+		else:
+			# The supplied form contained errors -
+			# just rint them to the terminal.
+			print(form.errors)
+	# Will handle the bad form, new form, or no form supplied cases
+	# Render the form with error messages (if any)
+	return render(request, 'rango/add_category.html', {'form':form})
 	
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
