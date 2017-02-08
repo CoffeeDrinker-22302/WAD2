b0764ca5cb5c3acc6f0d5a12eb5501625662dd85
Jimmy Tang <2301714T@student.gla.ac.uk>
2017-02-08 22:58:45 +0000

    finished chapter 10, need to attempt testing

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,6 +9,7 @@ from rango.forms import UserForm, UserProfileForm # Registering users
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime # Visitor_cookie_handler helper function.
 
 """	#############################################
 def detail(request, question_id):
@@ -75,7 +76,36 @@ def add_page(request, category_name_slug):
 
 	context_dict = {'form': form, 'category': category}
 	return render(request, 'rango/add_page.html', context_dict)
-			
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+	
+def visitor_cookie_handler(request):
+	# Get the number of visits to the site.
+	# We use the COOKIES.get() function to obtain the visits cookie.
+	# If the cookie exists, the value returned is casted to an integer.
+	# If the cookie doesn't exist, then the default value of 1 is used.
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+	
+	# If it's been more than a day since the last visit...
+	if(datetime.now() - last_visit_time).days>0:
+		visits = visits+1
+		#Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		visits = 1
+		# set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+	
+	# Update/set the visits cookie
+	request.session['visits'] = visits
+
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
 	# Order the categories by no. likes in descending order.
@@ -83,10 +113,15 @@ def index(request):
 	# Place the list in our context_dict dictionary
 	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes')[:5]
-	category_list_most_views = Category.objects.order_by('-views')
-	context_dict = {'categories': category_list,'most_liked':category_list_most_views}
-
-	# Render the response and send it back!
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list,'pages':page_list}
+	
+	# Call function to handle the cookies
+	visitor_cookie_handler(request)
+	
+	context_dict['visits'] = request.session['visits']
+	
+	# Return response back to the user, updating any cookies that need changed.
 	return render(request, 'rango/index.html', context_dict)
 	
 def show_category(request, category_name_slug):
